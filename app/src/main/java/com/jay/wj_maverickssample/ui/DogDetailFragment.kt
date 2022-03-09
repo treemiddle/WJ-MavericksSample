36471b4098348c8ecd775d011b63153a7e54013b
_____wj <wj122791@gmail.com>
2022-03-10 00:38:40 +0900

    초기 셋팅 및 ui 연결

---
+++ app/src/main/java/com/jay/wj_maverickssample/ui/DogDetailFragment.kt
@@ -0,0 +1,13 @@
+package com.jay.wj_maverickssample.ui
+
+import androidx.fragment.app.Fragment
+import com.airbnb.mvrx.MavericksView
+import com.jay.wj_maverickssample.R
+
+class DogDetailFragment : Fragment(R.layout.fragment_dog_detail), MavericksView {
+
+    override fun invalidate() {
+
+    }
+
+}
