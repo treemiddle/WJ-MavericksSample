aba81a577ee2327ebd7f5f94b0dc54f6e04093af
_____wj <wj122791@gmail.com>
2022-03-09 23:50:21 +0900

    first commit

---
+++ app/src/main/java/com/jay/wj_maverickssample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jay.wj_maverickssample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
